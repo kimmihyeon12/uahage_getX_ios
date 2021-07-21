c4e06ef3340c78bd5e0315c737dabac05087d79d
고재범 <gojaebeom@gojaebeom-ui-MacBookAir.local>
2021-07-21 10:08:23 +0900

    ui update

--- lib/src/Static/url.dart
+++ lib/src/Static/url.dart
@@ -1,2 +1,2 @@
-final String URL = "http://172.30.1.24:8000";
-//final String URL = "https://hohocompany.co.kr";
+//final String URL = "http://172.30.1.24:8000";
+final String URL = "https://hohocompany.co.kr";
